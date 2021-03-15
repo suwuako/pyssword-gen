cfed3ca08e78b4830d30b06639d9fac05ebd36db
Vincent <defspyvc41@yandex.com>
2021-03-15 01:58:57 -0400

    Add error-handling and more efficient structures
    This update will:
    - introduce a Set for the reservedIndices to prevent duplicates and increase lookup speed;
    - add error-handling when the user provides constraints that are impossible to satisfy;
    - polish the variable names (use CamelCase for class name, be more descriptive, etc.);
    - add a shebang to enable simpler execution, at least on Linux

--- generator.py
+++ generator.py
@@ -1,50 +1,51 @@
+#!/usr/bin/env python3
+# 
+# 
 import random
 import string
 
-class generatePassword:
+class GeneratePassword:
     def __init__(self):
         self.selection = string.ascii_letters
-        self.password = []
-        self.rngBag = []
+        self.password = ""
         
+        # a SET (dynamic, unordered collection, without repeated elements)
+        # storing indices that have already been modified and are therefore
+        # RESERVED (these elements must not be overwritten!)
+        self.reservedIndices = set()
         
-        
-    def display(self):
-        self.displayString = ''
-        
-        for i in self.password:
-            self.displayString += str(i)
-    
     def generateString(self):
         for i in range(self.totalLen):
             indexValue = random.choice(string.ascii_lowercase)
-            self.password.append(indexValue)
+            self.password += str(indexValue)
         
-        self.display()
-        
-        print(f'original: \n{self.displayString}\n')
+        print(f'original: \n{self.password}\n')
         
     def customize(self, selection, swapType, cap):
-        self.i = 0
-        while self.i != cap:
+        i = 0
+        while i < cap and len(self.reservedIndices) < len(self.password):
             index = random.randint(0, self.totalLen-1)
             
-            if index not in self.rngBag:
+            if index not in self.reservedIndices:
                 select = random.choice(selection)
                 print(f'replacing {self.password[index]} (index {index}) with {select}')
-                self.password[index] = select
-                self.rngBag.append(index)
-                self.i += 1
-            
+                updated_password = self.password[0:index] + select + self.password[(index+1):]
+                self.password = updated_password
+                self.reservedIndices.add(index)
+                i += 1
         
-        self.display()
-        print(f'after {swapType}:\n{self.displayString}')
+        if (i < cap): # len(self.reservedIndices) == len(self.password)):
+            print(f'ERR -- could not finish applying {swapType} constraint!')
+            print(f'    -- the number of reserved indices ({len(self.reservedIndices)}) apparently leaves no more room to make substitutions.')
+            exit(1)
+        else:
+            print(f'after applying {swapType}:\n{self.password}')
 
     def getUserInput(self):
         self.totalLen = int(input('How long do u want the password to be? '))
-        self.capsAmount = int(input('How many caps do u want? '))
-        self.numberAmount = int(input('How many numbers do u want? '))
-        self.symbolAmount = int(input('How many symbols do u want? '))
+        self.capsAmount = int(input('How many caps do you want? '))
+        self.numberAmount = int(input('How many numbers do you want? '))
+        self.symbolAmount = int(input('How many symbols do you want? '))
     
     def run(self):
         self.getUserInput()
@@ -52,9 +53,8 @@ class generatePassword:
         self.customize(string.digits, 'Numbers', self.capsAmount)
         self.customize(string.ascii_uppercase, 'Caps', self.numberAmount)
         self.customize(string.punctuation, 'Symbols', self.symbolAmount)
-
-        
     
 if __name__ == '__main__':
-    gp = generatePassword()
+    gp = GeneratePassword()
     gp.run()
+
